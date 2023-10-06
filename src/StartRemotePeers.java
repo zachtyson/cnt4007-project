33d9dc2c4a1efb2c36e59d59a074b4c7e780ae8b
Zachary <tysonzachary@ufl.edu>
2023-10-06 02:27:43 -0400

    changed back due to lack of common qualities that peerProcess can use

--- src/StartRemotePeers.java
+++ src/StartRemotePeers.java
@@ -62,4 +62,17 @@ public class StartRemotePeers {
             System.out.println(ex);
         }
     }
+
+    public static class RemotePeerInfo {
+        public String peerId;
+        public String peerAddress;
+        public String peerPort;
+
+        public RemotePeerInfo(String peerId, String peerAddress, String peerPort) {
+            this.peerId = peerId;
+            this.peerAddress = peerAddress;
+            this.peerPort = peerPort;
+        }
+    }
+
 }
