66e81c7be0a20ef9a3a096bd2adb3186510bd800
daa729 <daa729@gmail.com>
2023-10-24 15:45:15 -0400

    Update Host.java

--- src/Host.java
+++ src/Host.java
@@ -7,21 +7,30 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
+
+
 public class Host {
+    int PortLimit = 0;
+    // each message sent 
     ArrayList<Boolean> Message = new ArrayList<Boolean>();
-    public void listen(int port) throws UnknownHostException, IOException{
-        //listen to port
-        ServerSocket serverSocket = new ServerSocket(port);
+    // the different clients that are connected to the host 
+    ArrayList<Socket> Clients = new ArrayList<Socket>();
+    ServerSocket serverSocket; 
+    public Host(int port, int PortLimit)throws UnknownHostException, IOException
+    {
+        serverSocket = new ServerSocket(port);
+        this.PortLimit = PortLimit;
+        listen();
+    }
 
-        // can use threads and while loop to represent all connections to the host 
-        // array of clients each requesting something different from the host 
-        // write to these with our different mesages that are written in message.java 
-        Socket Client = serverSocket.accept();
-        PrintWriter in  = new PrintWriter(Client.getOutputStream(), true);
-        BufferedReader out = new BufferedReader( new InputStreamReader(Client.getInputStream()));
+    public void listen() throws UnknownHostException, IOException{
+        while(PortLimit>Clients.size())
+        {
+            Socket Client = serverSocket.accept();
+            Clients.add(Client);
            
-        
-
+        }
+       
     } 
 
 
