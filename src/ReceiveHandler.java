831f1c34fa84fd87608d7accd5f27ae073c38153
Zachary <tysonzachary@ufl.edu>
2023-11-16 22:00:30 -0500

    test code that doesn't work

--- src/ReceiveHandler.java
+++ src/ReceiveHandler.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 //ReceiveHandler is in charge of receiving messages from the peer, and passing them to the host process
 //and this happens by modifying the PeerConnection object's ConcurrentHashMap of byte[] and status enum
@@ -9,20 +10,87 @@ public class ReceiveHandler extends Thread{
     }
     @Override
     public void run() {
+        if(peerConnection.socket == null) {
+            System.out.println("Error: socket is null, verify that the peer is in the correct order in PeerInfo.cfg and that currentPeer is set correctly");
+        }
+        if(peerConnection.in == null) {
+            System.out.println("Error: in is null, verify that the peer is in the correct order in PeerInfo.cfg and that currentPeer is set correctly");
+        }
+        if(peerConnection.out == null) {
+            System.out.println("Error: out is null, verify that the peer is in the correct order in PeerInfo.cfg and that currentPeer is set correctly");
+        }
         System.out.println("Starting receive handler for peer " + peerConnection.peerId);
+        while (true) {
+            try {
+                byte[] message = receiveMessageLength();
+                if(message.length == 0) {
+                    System.out.println("Received keep alive message from peer " + peerConnection.peerId);
+                    continue;
+                }
+                System.out.println("Received message from peer " + peerConnection.peerId);
+                System.out.println("Message length: " + message.length);
+                int payloadLength = message.length - 5; //subtract 5 for the 4 byte length header and the message type
+                Message.Interpretation interpretation = Message.msgInterpret(message);
+                if(interpretation.Msg == MsgType.piece) {
+                    peerConnection.sendResponses.add(interpretation.pieceIndex);
+                    peerConnection.hostProcess.pieceData.put(interpretation.pieceIndex, interpretation.messagePayload);
+                    peerConnection.hostProcess.pieceMap.put(interpretation.pieceIndex, peerProcess.pieceStatus.DOWNLOADED);
+                }
+                if(interpretation.Msg == MsgType.have) {
+                    peerConnection.pieceMap.put(interpretation.pieceIndex, peerProcess.pieceStatus.DOWNLOADED);
+                }
+//                if (payloadLength < 0) {
+//                    System.out.println("Received message from peer " + peerConnection.peerId);
+//                    System.out.println("Message type: " + interpretation.Msg);
+//                    System.out.println("Payload length: " + interpretation.payloadLength);
+//                    System.out.println("Payload: " + interpretation.messagePayload);
+//                    throw new IOException("Payload length is negative");
+//
+//                }
+//                System.out.println("Received message from peer " + peerConnection.peerId);
+//                System.out.println("Message type: " + interpretation.Msg);
+//                System.out.println("Payload length: " + interpretation.payloadLength);
+//                System.out.println("Payload: " + interpretation.messagePayload);
+                // Process the message here
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
     }
     byte[] receiveMessageLength() throws IOException {
         // Each message (given from specifications) begins with a 4 byte length header
         // This method reads the length header and returns the message
-        int expectedLength = peerConnection.in.read();  // Assumes a 4-byte length header
-        return receiveMessage(expectedLength);
+        System.out.println("Received message from peerS " + peerConnection.peerId);
+        byte[] expectedLength = peerConnection.in.readNBytes(4);
+        ByteBuffer wrapped = ByteBuffer.wrap(expectedLength);
+        int expectedLengthInt = -1;
+        try {
+            expectedLengthInt = wrapped.getInt();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error: " + e.getStackTrace());
+            System.out.println("Error: " + e.getCause());
+            System.out.println("Error: " + e.getLocalizedMessage());
+            System.out.println("Error: " + e.getSuppressed());
+        }
+                // Assumes a 4-byte length header
+        if(expectedLengthInt == -1) {
+            throw new IOException("Connection was terminated before message was complete.");
+        }
+        if(expectedLengthInt == 0) {
+            return new byte[0];
+        }
+        System.out.println("Message length: " + expectedLengthInt);
+        return receiveMessage(expectedLengthInt);
     }
 
     byte[] receiveMessage(int expectedLength) throws IOException {
         // Read message of length expectedLength bytes
-        byte[] message = new byte[expectedLength]; //add +1 later for message type?
+        byte[] message = new byte[expectedLength+1]; //add +1 later for message type?
         int offset = 0;
-
+        byte messageType = (byte) peerConnection.in.read();
         while (offset < expectedLength) {
             int bytesRead = peerConnection.in.read(message, offset, expectedLength - offset);
             if (bytesRead == -1) {
--- src/SendHandler.java
+++ src/SendHandler.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class SendHandler extends Thread {
     PeerConnection peerConnection;
@@ -8,17 +9,107 @@ public class SendHandler extends Thread {
     @Override
     public void run() {
         System.out.println("Starting send handler for peer " + peerConnection.peerId);
+        int numMessages = 0;
+        while (true) {
+            if(!peerConnection.sendResponses.isEmpty()) {
+                //Send have message
+                int pieceIndex = peerConnection.sendResponses.remove();
+                byte[] message = Message.generateHaveMessage(pieceIndex);
+                try {
+                    sendMessage(message);
+                    System.out.println("Sent message to peer " + peerConnection.peerId);
+                    numMessages++;
+                    System.out.println("Number of messages sent to peer " + peerConnection.peerId + ": " + numMessages);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-    }
-    void sendMessage(byte[] msg) {
-        try {
-            //stream write the message
-            peerConnection.out.write(msg);
-            peerConnection.out.flush();
-            System.out.println("Sent message to peer " + peerConnection.peerId);
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+            }
+            boolean pieceWeHaveButTheyDont = false;
+            for(int i = 0; i < peerConnection.commonCfg.numPieces; i++) {
+                boolean hasPiece = false;
+                if(peerConnection.hostProcess.pieceMap.get(i) != peerProcess.pieceStatus.EMPTY) {
+                    hasPiece = true;
+                }
+                if(hasPiece) {
+                    //Check to see if other peer has piece
+                    if (peerConnection.pieceMap.get(i) == peerProcess.pieceStatus.EMPTY) {
+                        pieceWeHaveButTheyDont = true;
+                        //Send interested message
+//                    byte[] message = Message.generateInterestedMessage();
+//                    sendMessage(message);
+//                    System.out.println("Sent message to peer " + peerConnection.peerId);
+//                    numMessages++;
+//                    System.out.println("Number of messages sent to peer " + peerConnection.peerId + ": " + numMessages);
+                    }
+                }
+            }
+            if(!pieceWeHaveButTheyDont) {
+                return;
+            }
+            for(int i = 0; i < peerConnection.commonCfg.numPieces; i++) {
+                boolean hasPiece = false;
+                if(peerConnection.hostProcess.pieceMap.get(i) != peerProcess.pieceStatus.EMPTY) {
+                    hasPiece = true;
+                }
+                if(hasPiece) {
+                    //Check to see if other peer has piece
+                    if (peerConnection.pieceMap.get(i) == peerProcess.pieceStatus.EMPTY) {
+                        peerConnection.requestedPieces.add(i);
+                    }
+                }
+            }
+            //Check peer's bitfield to see if there are any pieces that we don't have
+            //If there are, send an interested message
+            try {
+                // Wait 1 second
+                Thread.sleep(10000);
+//                byte[] message = Message.generateBitmapMessage(peerConnection.hostProcess.pieceMap, peerConnection.commonCfg.numPieces);
+//                sendMessage(message);
+//                System.out.println("Sent message to peer " + peerConnection.peerId);
+//                numMessages++;
+//                System.out.println("Number of messages sent to peer " + peerConnection.peerId + ": " + numMessages);
+                //Send requestedPiece top of queue
+                int requestedPiece = peerConnection.requestedPieces.remove();
+                byte[] piece = peerConnection.hostProcess.pieceData.get(requestedPiece);
+                byte[] message = Message.generatePieceMessage(piece);
+                byte[] lengthAndType = Message.generateHeaderAndMessageType(message.length, MsgType.piece);
+//                int messageLength = message.length;
+//                byte[] messageLengthBytes = ByteBuffer.allocate(4).putInt(messageLength).array();
+//                System.out.println("Sending piece " + requestedPiece + " to peer " + peerConnection.peerId);
+//                System.out.println(message.length);
+//                byte[] overallMessage = new byte[messageLength + 4];
+//                System.arraycopy(messageLengthBytes, 0, overallMessage, 0, 4);
+//                System.arraycopy(message, 0, overallMessage, 4, messageLength);
+//                Message.Interpretation interpretation = Message.msgInterpret(overallMessage);
+//                System.out.println("Message type: " + interpretation.Msg);
+//                System.out.println("Payload length: " + interpretation.payloadLength);
+//                System.out.println("Payload: " + interpretation.messagePayload);
+                byte[] overallMessage = new byte[lengthAndType.length + message.length+4]; //+ 4 for the 4-byte piece index field
+                System.arraycopy(lengthAndType, 0, overallMessage, 0, lengthAndType.length);
+                System.arraycopy(message, 0, overallMessage, lengthAndType.length, message.length);
+                byte[] pieceIndex = ByteBuffer.allocate(4).putInt(requestedPiece).array();
+                System.arraycopy(pieceIndex, 0, overallMessage, lengthAndType.length + message.length, 4);
+                byte[] firstFourBytes = new byte[4];
+                System.arraycopy(overallMessage, 0, firstFourBytes, 0, 4);
+                int test = (int) ByteBuffer.wrap(firstFourBytes).getInt();
+                byte test2 = overallMessage[4];
+                Message.Interpretation interpretation = Message.msgInterpret(overallMessage);
+                System.out.println("Sending piece " + requestedPiece + " to peer " + peerConnection.peerId);
+
+                sendMessage(overallMessage);
+                // Process the message here
+            } catch (Exception e) {
+                return;
+            }
+
         }
+    }
+    void sendMessage(byte[] msg) throws IOException{
+        //stream write the message
+        peerConnection.out.write(msg);
+        peerConnection.out.flush();
+
     }
 
 }
