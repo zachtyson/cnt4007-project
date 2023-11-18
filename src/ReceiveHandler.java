20ec95ce484810a10fde84677f3942c62adc3b86
Zachary <tysonzachary@ufl.edu>
2023-11-18 14:50:50 -0500

    logic for receiving and interpreting incoming messages

--- src/ReceiveHandler.java
+++ src/ReceiveHandler.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 //ReceiveHandler is in charge of receiving messages from the peer, and passing them to the host process
 //and this happens by modifying the PeerConnection object's ConcurrentHashMap of byte[] and status enum
@@ -10,51 +11,79 @@ public class ReceiveHandler extends Thread{
     }
     @Override
     public void run() {
-        if(peerConnection.socket == null) {
-            System.out.println("Error: socket is null, verify that the peer is in the correct order in PeerInfo.cfg and that currentPeer is set correctly");
-        }
-        if(peerConnection.in == null) {
-            System.out.println("Error: in is null, verify that the peer is in the correct order in PeerInfo.cfg and that currentPeer is set correctly");
-        }
-        if(peerConnection.out == null) {
-            System.out.println("Error: out is null, verify that the peer is in the correct order in PeerInfo.cfg and that currentPeer is set correctly");
-        }
-        System.out.println("Starting receive handler for peer " + peerConnection.peerId);
         while (true) {
             try {
                 byte[] message = receiveMessageLength();
-                if(message.length == 0) {
-                    System.out.println("Received keep alive message from peer " + peerConnection.peerId);
-                    continue;
+                if (message.length == 0) {
+                    System.out.println("Received keep alive message from peer");
+                    // continue;
                 }
-                System.out.println("Received message from peer " + peerConnection.peerId);
-                System.out.println("Message length: " + message.length);
-                int payloadLength = message.length - 5; //subtract 5 for the 4 byte length header and the message type
+                //System.out.println(message.length);
+                clearBuffer();
+                System.out.println("Received message: " + Arrays.toString(message));
                 Message.Interpretation interpretation = Message.msgInterpret(message);
-                if(interpretation.Msg == MsgType.piece) {
-                    peerConnection.sendResponses.add(interpretation.pieceIndex);
-                    peerConnection.hostProcess.pieceData.put(interpretation.pieceIndex, interpretation.messagePayload);
-                    peerConnection.hostProcess.pieceMap.put(interpretation.pieceIndex, peerProcess.pieceStatus.DOWNLOADED);
-                }
-                if(interpretation.Msg == MsgType.have) {
-                    peerConnection.pieceMap.put(interpretation.pieceIndex, peerProcess.pieceStatus.DOWNLOADED);
-                }
-//                if (payloadLength < 0) {
-//                    System.out.println("Received message from peer " + peerConnection.peerId);
-//                    System.out.println("Message type: " + interpretation.Msg);
-//                    System.out.println("Payload length: " + interpretation.payloadLength);
-//                    System.out.println("Payload: " + interpretation.messagePayload);
-//                    throw new IOException("Payload length is negative");
-//
+//                if(interpretation.Msg == MsgType.bitfield) {
+//                    //Bitwise, set the pieces that the peer has
+//                    for(int i = 0; i < peerConnection.numPieces; i++) {
+//                        int nthBit = Message.getNthBit(interpretation.messagePayload, i);
+//                        if(nthBit == 1) {
+//                            peerConnection.pieceMap.put(i, Main.pieceStatus.DOWNLOADED);
+//                        }
+//                    }
 //                }
-//                System.out.println("Received message from peer " + peerConnection.peerId);
-//                System.out.println("Message type: " + interpretation.Msg);
-//                System.out.println("Payload length: " + interpretation.payloadLength);
-//                System.out.println("Payload: " + interpretation.messagePayload);
-                // Process the message here
+                switch(interpretation.Msg) {
+                    case choke:
+                        System.out.println("Received choke message from peer");
+                        break;
+                    case unchoke:
+                        System.out.println("Received unchoke message from peer");
+                        break;
+                    case interested:
+                        System.out.println("Received interested message from peer");
+                        break;
+                    case have:
+                        System.out.println("Received have message from peer");
+                        break;
+                    case request:
+                        System.out.println("Received request message from peer");
+                        System.out.println("Piece index: " + interpretation.pieceIndex);
+
+                        break;
+                    case piece:
+                        System.out.println("Received piece message from peer");
+                        break;
+                    case bitfield:
+                        System.out.println("Received bitfield message from peer");
+                        for(int i = 0; i < peerConnection.commonCfg.numPieces; i++) {
+                            int nthBit = Message.getNthBit(interpretation.messagePayload, i);
+                            if(nthBit == 1) {
+                                peerConnection.peerPieceMap.put(i, peerProcess.pieceStatus.DOWNLOADED);
+                            }
+                        }
+                        //If entire bitfield is Downloaded, set hasAllPieces to true
+                        //Honestly not entirely familiar with the specs so not sure if a peer would ever send a bitfield if they don't have all the pieces
+                        //But I guess it's possible so it'll make this code a bit less efficient
+                        boolean hasAllPieces = true;
+                        for(int i = 0; i < peerConnection.commonCfg.numPieces; i++) {
+                            if(peerConnection.peerPieceMap.get(i) != peerProcess.pieceStatus.DOWNLOADED) {
+                                hasAllPieces = false;
+                                break;
+                            }
+                        }
+                        peerConnection.peerHasAllPieces.set(hasAllPieces);
+                        for(int i = 0; i < peerConnection.commonCfg.numPieces; i++) {
+                            System.out.println("Piece " + i + " is " + peerConnection.peerPieceMap.get(i));
+                        }
+                        if(hasAllPieces) {
+                            System.out.println("Peer has all pieces");
+                        }
+                        break;
+                    default:
+                        System.out.println("Received unknown message from peer");
+                        break;
+                }
             } catch (IOException e) {
-                e.printStackTrace();
-                break;
+                //e.printStackTrace();
             }
         }
     }
@@ -100,5 +129,12 @@ public class ReceiveHandler extends Thread{
         }
 
         return message;
+    }
+
+    void clearBuffer() throws IOException {
+        int bytesAvailable = peerConnection.in.available();
+        if (bytesAvailable > 0) {
+            peerConnection.in.skip(bytesAvailable);
+        }
     }
 }
